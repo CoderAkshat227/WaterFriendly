0b27cbf2c5f05b0050cc68092ee1f334706a4688
Akshat Bohre <akshatplays@gmail.com>
2021-06-14 08:54:50 +0800

    First Commit

---
+++ MainProject/BSMHackathon/BSMHackathon/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using BSMHackathon.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace BSMHackathon.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
